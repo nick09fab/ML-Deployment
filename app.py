31ac7ecd2da75b12c0b6bd9c38f4d1e682351c04
Nikhil <nikhil@dqlabs.ai>
2023-02-09 16:00:21 +0530

    Flask App

---
+++ app.py
@@ -0,0 +1,21 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+@flask_app.route("/")
+def home():
+    return render_template("index.html")
+
+@flask_app.route("/predSSict", methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
